755d24ec698cb4aa4cc17b8c771bfd2dbcbedc10
Dmitry Kurtaev <dmitry.kurtaev+github@gmail.com>
2020-02-08 14:42:33 +0300

    fix compile error

--- project/src/roberts.cpp
+++ project/src/roberts.cpp
@@ -4,7 +4,7 @@ void roberts_reference(const cv::Mat& src, cv::Mat& dst) {
     CV_Assert(src.type() == CV_8UC1);
     Mat bsrc;
     copyMakeBorder(src, bsrc, 1, 1, 1, 1, BORDER_REPLICATE);
-    dst.create(src.size(), CV_32UC1);
+    dst.create(src.size(), CV_32SC1);
     for (int y = 0; y < dst.rows; ++y)
         for (int x = 0; x < dst.cols; ++x) {
             int dx = bsrc.at<uchar>(y, x) - bsrc.at<uchar>(y + 1, x + 1);
