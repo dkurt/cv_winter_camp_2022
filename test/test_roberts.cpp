63d208c41d7845cf199d4c63c1a1b05257bbaf40
Andrey <okm.andrey@yandex.ru>
2021-02-05 15:02:46 +0300

    added roberts_parallel_vec

--- test/test_roberts.cpp
+++ test/test_roberts.cpp
@@ -14,4 +14,17 @@ TEST_P(Roberts, parallel)
     EXPECT_EQ(countNonZero(ref != dst), 0);
 }
 
+
+typedef testing::TestWithParam<Size> Roberts;
+TEST_P(Roberts, parallel_vec)
+{
+    cv::Mat src(GetParam(), CV_8UC1), ref, dst;
+    randu(src, 0, 127);
+
+    roberts_reference(src, ref);
+    roberts_parallel_vec(src, dst);
+
+    EXPECT_EQ(countNonZero(ref != dst), 0);
+}
+
 INSTANTIATE_TEST_CASE_P(/**/, Roberts, testing::Values(TYPICAL_MAT_SIZES));
