d95a88638465ec1680960e2cc278bae53d85f0a1
ilyaShein <ilya.shein.1999@mail.ru>
2021-02-05 13:55:01 +0300

    new parallel

---
+++ build/perf_roberts.cpp
@@ -0,0 +1,26 @@
+#include <opencv2/ts.hpp>
+
+#include "algo.hpp"
+
+
+PERF_TEST(Roberts, ref)
+{
+    cv::Mat src(1080, 1920, CV_8UC1), dst;
+
+    PERF_SAMPLE_BEGIN()
+        roberts_reference(src, dst);
+    PERF_SAMPLE_END()
+
+        SANITY_CHECK_NOTHING();
+}
+
+PERF_TEST(Roberts, parallel)
+{
+    cv::Mat src(1080, 1920, CV_8UC1), dst;
+
+    PERF_SAMPLE_BEGIN()
+        roberts_parallel(src, dst);
+    PERF_SAMPLE_END()
+
+        SANITY_CHECK_NOTHING();
+}
