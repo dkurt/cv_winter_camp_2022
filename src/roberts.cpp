f7535c5fbebe5e5e5e28b8b31bfdf56db457bd9c
381706-1-DenisovVladislavL <vlad.roix@yandex.ru>
2021-02-05 17:39:32 +0700

    Roberts - parallel & vector_wrap + tests

--- src/roberts.cpp
+++ src/roberts.cpp
@@ -1,4 +1,5 @@
 #include "algo.hpp"
+#include <iostream>
 
 void roberts_reference(const cv::Mat& src, cv::Mat& dst) {
     CV_Assert(src.type() == CV_8UC1);
@@ -11,4 +12,47 @@ void roberts_reference(const cv::Mat& src, cv::Mat& dst) {
             int dy = bsrc.at<uchar>(y, x + 1) - bsrc.at<uchar>(y + 1, x);
             dst.at<uint32_t>(y, x) = dx * dx + dy * dy;
         }
+}
+
+void roberts_parallel(const Mat& src, Mat& dst) {
+    Mat bsrc;
+    copyMakeBorder(src, bsrc, 1, 1, 1, 1, BORDER_REPLICATE);
+    dst.create(src.size(), CV_32SC1);
+    parallel_for_(Range(0, src.rows), [&](const Range& range) {
+        for (int y = range.start; y < range.end; ++y)
+            for (int x = 0; x < dst.cols; ++x) {
+                int dx = bsrc.at<uchar>(y, x) - bsrc.at<uchar>(y + 1, x + 1);
+                int dy = bsrc.at<uchar>(y, x + 1) - bsrc.at<uchar>(y + 1, x);
+                dst.at<uint32_t>(y, x) = dx * dx + dy * dy;
+            }
+    });
+}
+
+#include <opencv2/core/hal/intrin.hpp>
+
+void roberts_parallel_vec_wrap(const Mat& src, Mat& dst) {
+    Mat bsrc;
+    copyMakeBorder(src, bsrc, 1, 1, 1, 1, BORDER_REPLICATE);
+    dst.create(src.size(), CV_32SC1);
+    parallel_for_(Range(0, src.rows), [&](const Range& range) {
+        for (int y = range.start; y < range.end; ++y) {
+            const uint8_t* psrc0 = bsrc.ptr(y);
+            const uint8_t* psrc1 = bsrc.ptr(y + 1);
+            uint8_t* pdst = dst.ptr(y);
+            int x = 0;
+            for (; x <= dst.cols - v_uint8::nlanes; x += v_uint8::nlanes) {
+                v_uint8 res = v_add_wrap(
+                    v_mul_wrap(v_sub_wrap(vx_load(psrc0 + x), vx_load(psrc1 + x + 1)), 
+                               v_sub_wrap(vx_load(psrc0 + x), vx_load(psrc1 + x + 1))),
+                    v_mul_wrap(v_sub_wrap(vx_load(psrc0 + x + 1), vx_load(psrc1 + x)),
+                               v_sub_wrap(vx_load(psrc0 + x + 1), vx_load(psrc1 + x)))
+                );
+                v_store(pdst + x, res);
+            }
+            for (; x < dst.cols; ++x) {
+                pdst[x] = (psrc0[x] - psrc1[x + 1]) * (psrc0[x] - psrc1[x + 1]) +
+                    (psrc0[x + 1] - psrc1[x]) * (psrc0[x + 1] - psrc1[x]);
+            }
+        }
+    });
 }
