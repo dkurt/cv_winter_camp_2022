a0c9e02610e2682cf7c729f4580213ca59d047e6
dimen2000 <dimentor2000@yandex.ru>
2021-02-05 15:27:36 +0300

    parallel and vectorization

--- src/roberts.cpp
+++ src/roberts.cpp
@@ -4,11 +4,53 @@ void roberts_reference(const cv::Mat& src, cv::Mat& dst) {
     CV_Assert(src.type() == CV_8UC1);
     Mat bsrc;
     copyMakeBorder(src, bsrc, 1, 1, 1, 1, BORDER_REPLICATE);
-    dst.create(src.size(), CV_32SC1);
+    dst.create(src.size(), CV_16SC1);
     for (int y = 0; y < dst.rows; ++y)
         for (int x = 0; x < dst.cols; ++x) {
             int dx = bsrc.at<uchar>(y, x) - bsrc.at<uchar>(y + 1, x + 1);
             int dy = bsrc.at<uchar>(y, x + 1) - bsrc.at<uchar>(y + 1, x);
-            dst.at<uint32_t>(y, x) = dx * dx + dy * dy;
+            dst.at<uint16_t>(y, x) = dx * dx + dy * dy;
         }
+}
+
+void roberts_parallel(const cv::Mat& src, cv::Mat& dst) {
+    CV_Assert(src.type() == CV_8UC1);
+    Mat bsrc;
+    copyMakeBorder(src, bsrc, 1, 1, 1, 1, BORDER_REPLICATE);
+    dst.create(src.size(), CV_16SC1);
+    parallel_for_(Range(0, src.rows), [&](const Range& range) {
+        for (int y = range.start; y < range.end; ++y)
+            for (int x = 0; x < dst.cols; ++x) {
+                int dx = bsrc.at<uchar>(y, x) - bsrc.at<uchar>(y + 1, x + 1);
+                int dy = bsrc.at<uchar>(y, x + 1) - bsrc.at<uchar>(y + 1, x);
+                dst.at<uint16_t>(y, x) = dx * dx + dy * dy;
+            }
+    });
+}
+
+#include <opencv2/core/hal/intrin.hpp>
+
+void roberts_parallel_vec(const Mat& src, Mat& dst) {
+    Mat bsrc;
+    copyMakeBorder(src, bsrc, 1, 1, 1, 1, BORDER_REPLICATE);
+    dst.create(src.size(), CV_16SC1);
+    parallel_for_(Range(0, src.rows), [&](const Range& range) {
+        for (int y = range.start; y < range.end; ++y) {
+            const uint8_t* psrc0 = bsrc.ptr(y);
+            const uint8_t* psrc1 = bsrc.ptr(y + 1);
+            int16_t* pdst = dst.ptr<int16_t>(y);
+            int x = 0;
+            for (; x <= dst.cols - v_uint16::nlanes; x += v_uint16::nlanes) {
+                v_int16 dx = v_reinterpret_as_s16(vx_load_expand(psrc0 + x)) - v_reinterpret_as_s16(vx_load_expand(psrc1 + x + 1));
+                v_int16 dy = v_reinterpret_as_s16(vx_load_expand(psrc0 + x + 1)) - v_reinterpret_as_s16(vx_load_expand(psrc1 + x));
+                v_int16 res = dx * dx + dy * dy;
+                v_store(pdst + x, res);
+            }
+            for (; x < dst.cols; ++x) {
+                uint16_t dx = psrc0[x] - psrc1[x + 1];
+                uint16_t dy = psrc0[x + 1] - psrc1[x];
+                pdst[x] = dx * dx + dy * dy;
+            }
+        }
+    });
 }
---
+++ test/test_roberts.cpp
@@ -0,0 +1,27 @@
+#include <opencv2/ts.hpp>
+
+#include "algo.hpp"
+
+typedef testing::TestWithParam<Size> Roberts;
+TEST_P(Roberts, parallel)
+{
+    cv::Mat src(GetParam(), CV_8UC1), ref, dst;
+    randu(src, 0, 255);
+
+    roberts_reference(src, ref);
+    roberts_parallel(src, dst);
+
+    EXPECT_EQ(countNonZero(ref != dst), 0);
+}
+
+ TEST_P(Roberts, parallel_vec)
+ {
+     cv::Mat src(GetParam(), CV_8UC1), ref, dst;
+     randu(src, 0, 127);
+
+     roberts_reference(src, ref);
+     roberts_parallel_vec(src, dst);
+
+     EXPECT_EQ(countNonZero(ref != dst), 0);
+ }
+INSTANTIATE_TEST_CASE_P(/**/, Roberts, testing::Values(TYPICAL_MAT_SIZES));
