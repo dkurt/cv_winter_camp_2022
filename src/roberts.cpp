73cd363953e10ba7ecdec8a36c1210543ccdfc88
danek0100 <71225718+danek0100@users.noreply.github.com>
2021-02-05 14:00:41 +0300

    parallel

--- src/roberts.cpp
+++ src/roberts.cpp
@@ -12,3 +12,17 @@ void roberts_reference(const cv::Mat& src, cv::Mat& dst) {
             dst.at<uint32_t>(y, x) = dx * dx + dy * dy;
         }
 }
+
+void roberts_parallel(const cv::Mat& src, cv::Mat& dst) {
+    Mat bsrc;
+    copyMakeBorder(src, bsrc, 1, 1, 1, 1, BORDER_REPLICATE);
+    dst.create(src.size(), CV_32SC1);
+    parallel_for_(Range(0, src.rows), [&](const Range& range) {
+        for (int y = range.start; y < range.end; ++y)
+            for (int x = 0; x < dst.cols; ++x) {
+                int dx = bsrc.at<uchar>(y, x) - bsrc.at<uchar>(y + 1, x + 1);
+                int dy = bsrc.at<uchar>(y, x + 1) - bsrc.at<uchar>(y + 1, x);
+                dst.at<uint32_t>(y, x) = dx * dx + dy * dy;
+            }
+        });
+}
