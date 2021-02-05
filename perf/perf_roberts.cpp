10aef629d6af28054afcb527c28e9b471fcbffb0
Alexey42 <550001@mail.ru>
2021-02-05 15:51:53 +0300

    Roberts. Par+vec+tests

--- perf/perf_roberts.cpp
+++ perf/perf_roberts.cpp
@@ -4,55 +4,33 @@
 
 PERF_TEST(Roberts, ref)
 {
-    cv::Mat src(1080, 1920, CV_8UC1), dst;
+  cv::Mat src(1080, 1920, CV_8UC1), dst;
 
-    PERF_SAMPLE_BEGIN()
-        roberts_reference(src, dst);
-    PERF_SAMPLE_END()
+  PERF_SAMPLE_BEGIN()
+    roberts_reference(src, dst);
+  PERF_SAMPLE_END()
 
     SANITY_CHECK_NOTHING();
 }
 
 PERF_TEST(Roberts, parallel)
 {
-    cv::Mat src(1080, 1920, CV_8UC1), dst;
+  cv::Mat src(1080, 1920, CV_8UC1), dst;
 
-    PERF_SAMPLE_BEGIN()
-        roberts_parallel(src, dst);
-    PERF_SAMPLE_END()
-
-    SANITY_CHECK_NOTHING();
-}
-/*
-PERF_TEST(Prewitt, parallel_vec)
-{
-    cv::Mat src(1080, 1920, CV_8UC1), dst;
-
-    PERF_SAMPLE_BEGIN()
-        prewitt_x_parallel_vec(src, dst);
-    PERF_SAMPLE_END()
+  PERF_SAMPLE_BEGIN()
+    roberts_parallel(src, dst);
+  PERF_SAMPLE_END()
 
     SANITY_CHECK_NOTHING();
 }
 
-PERF_TEST(Prewitt, parallel_vec_wrap)
+PERF_TEST(Roberts, parallel_vector)
 {
-    cv::Mat src(1080, 1920, CV_8UC1), dst;
+  cv::Mat src(1080, 1920, CV_8UC1), dst;
 
-    PERF_SAMPLE_BEGIN()
-        prewitt_x_parallel_vec_wrap(src, dst);
-    PERF_SAMPLE_END()
+  PERF_SAMPLE_BEGIN()
+    roberts_parallel_vector(src, dst);
+  PERF_SAMPLE_END()
 
     SANITY_CHECK_NOTHING();
 }
-
-PERF_TEST(Prewitt, parallel_vec_wrap2)
-{
-    cv::Mat src(1080, 1920, CV_8UC1), dst;
-
-    PERF_SAMPLE_BEGIN()
-        prewitt_x_parallel_vec_wrap2(src, dst);
-    PERF_SAMPLE_END()
-
-    SANITY_CHECK_NOTHING();
-}*/
